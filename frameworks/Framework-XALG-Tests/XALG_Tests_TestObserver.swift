39d7494455877007626426750951ad524345712e
Juguang XIAO <juguang@gmail.com>
2017-05-21 19:47:38 +0800

    TestObserver

---
+++ frameworks/Framework-XALG-Tests/XALG_Tests_TestObserver.swift
@@ -0,0 +1,40 @@
+//
+//  XALG_Tests_TestObserver.swift
+//  XALG
+//
+//  Created by Juguang Xiao on 17/05/2017.
+//
+
+import XCTest
+
+// https://developer.apple.com/videos/play/wwdc2016/409/
+// https://developer.apple.com/reference/xctest/xctestobservationcenter
+// https://developer.apple.com/reference/xctest/xctestobservation
+class XALG_Tests_TestObserver: NSObject, XCTestObservation {
+
+    override init() {
+        super.init()
+        
+        XCTestObservationCenter.shared().addTestObserver(self)
+        
+    }
+    
+    
+    func testBundleWillStart(_ testBundle: Bundle) {
+        print("testBundleWillStart")
+    }
+    
+    func testSuiteWillStart(_ testSuite: XCTestSuite) {
+        print("testSuiteWillStart")
+    }
+    
+    
+    func testSuiteDidFinish(_ testSuite: XCTestSuite) {
+        
+    }
+    
+    func testBundleDidFinish(_ testBundle: Bundle) {
+        
+    }
+    
+}
